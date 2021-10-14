d90eeca6064879fbe8b273ff3ba4fb5023565076
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2021-10-14 13:08:42 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,5 +2,7 @@ from IPython.display import Image
 import os
 # yolo 경로 수정해야 함
 %cd C:/Users/coco7/yolov5
+
+# test할 이미지 경로도 따로 지정해줘야 함.
 test_img_path = test_img_list[1]
 !python detect.py --weights ./runs/train/results21/weights/best.pt --img 416 --conf 0.5 --source {test_img_path}
