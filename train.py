9d8e9ff482aec7ff1fddb54cebb488b453625957
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2021-10-14 13:09:31 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
 # yolo 경로 수정해야 함
 %cd C:/Users/coco7/yolov5
+
+# 파라미터 값 중 데이터 경로도 따로 설정해줘야 함
 %run train.py --img 640 --batch 8 --epochs 50 --data C:/dataset/yz_tile_resize_split_blur/data.yaml --cfg ./models/yolov5s.yaml --weights yolov5s.pt --name results --device 0
