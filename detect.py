d36f4cec5eccf8f6c17cfa9fcc0459d5daeca09e
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2021-10-18 14:55:51 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,5 +4,6 @@ import os
 %cd C:/Users/coco7/yolov5
 
 # test할 이미지 경로도 따로 지정해줘야 함.
+# weights 경로는 학습할 때마다 매번 달라져서 매번 고쳐줘야 함.
 test_img_path = test_img_list[1]
 !python detect.py --weights ./runs/train/results21/weights/best.pt --img 416 --conf 0.5 --source {test_img_path}
